af32dddd5e052f52ad5e1810b69e537ff1d5574b
Alex Merced <alex@alexmerced.dev>
2024-01-06 11:55:42 -0500

    moved middleware

--- server.js
+++ server.js
@@ -3,38 +3,15 @@
 ///////////////////////////////////
 require("dotenv").config(); // load .env variables
 const express = require("express"); // our web framework
-const morgan = require("morgan"); // our logger
-const methodOverride = require("method-override"); // override forms
-const fruitController = require("./controllers/fruit")
-const userController = require("./controllers/user")
-const session = require("express-session")
-const MongoStore = require("connect-mongo")
+const registerGlobalMiddleware = require("./utils/middleware");
 
 //////////////////////////////////////////////////
 // Express App Object
 /////////////////////////////////////////////////
 const app = express();
 
-////////////////////////////////////////////////////
-// Register our Middleware
-////////////////////////////////////////////////////
-//normal middleware
-app.use(morgan("dev")); //logger
-app.use(methodOverride("_method")); // override form submissions
-app.use(express.urlencoded({ extended: true })); // parse urlencoded bodies
-app.use(express.static("public")); // serve files from public folder
-app.use(session({
-  secret: process.env.SECRET,
-  store: MongoStore.create({mongoUrl: process.env.DATABASE_URL}),
-  saveUninitialized: true,
-  resave: false
-}))
-
-
-
-// routers
-app.use("/fruits", fruitController)
-app.use("/user", userController)
+// Register Middleware
+registerGlobalMiddleware(app);
 
 /////////////////////////////////////////////////////
 // Routes
